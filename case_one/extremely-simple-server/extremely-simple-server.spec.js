1c4dc2be649f4aecd73041c5d406b5bf7dbea906
woleuntem <2195407+woleuntem@users.noreply.github.com>
2017-09-05 07:54:56 +0100

    Update extremely-simple-server.spec.js

--- case_one/extremely-simple-server/extremely-simple-server.spec.js
+++ case_one/extremely-simple-server/extremely-simple-server.spec.js
@@ -1,7 +1,7 @@
 "use strict";
 const Assert = require("assert");
 const Http = require("http");
-const XServer = require("./extremely-simple-server.js")
+const XServer = require("./extremely-simple-server.js");
 
 describe("Module extremely-simple-server", function () {
     describe("create", function () {
@@ -9,21 +9,28 @@ describe("Module extremely-simple-server", function () {
             Assert.equal(XServer.create().constructor, Http.Server);
         });
 
-        it("http://127.0.0.1:8080/evil.exe should return a status 404", function (done) {
+        it("http://127.0.0.1:8080/evil.exe should return a status code of 404", function (done) {
             Http.get("http://127.0.0.1:8080/evil.exe", function (response) {
                 Assert.equal(response.statusCode, 404);
                 done();
             });
         });
 
-        it("http://127.0.0.1:8080/amd should return a status 200", function (done) {
+        it("http://127.0.0.1:8080/../evil.exe should return a status code of 404", function (done) {
+            Http.get("http://127.0.0.1:8080/../evil.exe", function (response) {
+                Assert.equal(response.statusCode, 404);
+                done();
+            });
+        });
+
+        it("http://127.0.0.1:8080/amd should return a status code of 200", function (done) {
             Http.get("http://127.0.0.1:8080/amd", function (response) {
                 Assert.equal(response.statusCode, 200);
                 done();
             });
         });
 
-        it("http://127.0.0.1:4343/amd should return a status 200", function (done) {
+        it("http://127.0.0.1:4343/amd should return a status code of 200", function (done) {
             XServer.create(4343);
             Http.get("http://127.0.0.1:4343/amd", function (response) {
                 Assert.equal(response.statusCode, 200);
