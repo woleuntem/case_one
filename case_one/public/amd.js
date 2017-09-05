ff6a6e5bf97fb72d24a820253686fe2fab9ad7c1
woleuntem <2195407+woleuntem@users.noreply.github.com>
2017-09-05 07:47:31 +0100

    Update amd.js

--- case_one/public/amd.js
+++ case_one/public/amd.js
@@ -1,5 +1,19 @@
+/*jslint
+    browser es6 maxlen:80
+*/
+/*global
+    Object window
+*/
+
+//https://github.com/amdjs/amdjs-api/blob/master/AMD.md
 (function () {
     "use strict";
 
-    console.log(123);
+    if (window.hasOwnProperty("define")) {
+        return;
+    }
+
+    const define = (/*id, dependencies, factory*/) => null;
+    Object.defineProperty(define, "amd", {enumerable: true, value: {}});
+    Object.defineProperty(window, "define", {enumerable: true, value: define});
 }());
