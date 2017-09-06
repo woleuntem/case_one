3317fa3b51460be4a4187a213aef4df2dda017d3
woleuntem <2195407+woleuntem@users.noreply.github.com>
2017-09-06 08:54:22 +0100

    Update amd.js

--- case_one/public/amd.js
+++ case_one/public/amd.js
@@ -2,7 +2,7 @@
     browser es6 maxlen:80
 */
 /*global
-    Object window
+    Map Object window
 */
 
 // https://github.com/amdjs/amdjs-api/blob/master/AMD.md
@@ -13,7 +13,51 @@
         return;
     }
 
-    const define = (/*id, dependencies, factory*/) => null;
-    Object.defineProperty(define, "amd", {enumerable: true, value: {}});
-    Object.defineProperty(window, "define", {enumerable: true, value: define});
+    const define = function (map) {
+        /*
+        const _dependencies = function (_arguments) {
+            return _arguments.find((argument) => Array.isArray(argument))
+                    || ["require", "exports", "module"];
+        };
+
+        const _factory = function (factory) {
+            return typeof factory === "function"
+                ? factory()
+                : factory;
+        };
+        */
+
+        const _id = function (id) {
+            return typeof id === "string"
+                ? id
+                : null;
+        };
+
+        return function (..._arguments) {
+            const id = _id(_arguments[0]);
+
+            if (map.has(id)) {
+                return;
+            }
+
+            /*
+            const dependencies = _dependencies(_arguments.slice(0, 2));
+            map.set(id, _factory(_arguments.slice(-1)[0]));
+            */
+        };
+    };
+
+    const store = new Map();
+
+    Object.defineProperty(
+        window,
+        "define",
+        {enumerable: true, value: define(store)}
+    );
+
+    Object.defineProperty(
+        window.define,
+        "amd",
+        {enumerable: true, value: {}}
+    );
 }());
