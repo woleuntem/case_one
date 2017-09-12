86150e19e2ae73ae8aeddd343a8bc314389dfba2
woleuntem <2195407+woleuntem@users.noreply.github.com>
2017-09-12 12:57:42 +0100

    Add files via upload

---
+++ case_one/directory_chess/index.js
@@ -0,0 +1,109 @@
+/*jslint
+    es6 maxlen:80 node
+*/
+"use strict";
+
+const Fs = require("fs");
+const Path = require("path");
+
+const flatten = function (array) {
+    return array.reduce((a, b) => a.concat(b), []);
+};
+
+const partApply = function (fn, ...fixed) {
+    return (...args) => fn.apply(null, [...fixed, ...args]);
+};
+
+const watch = function (path) {
+    Fs.watch(path, function (eventType, filename) {
+        console.log(path, eventType, filename);
+    });
+};
+
+const childDirectoryItemStatCallback = function (spec, ignore, stats) {
+    spec.promise.resolve(
+        stats.isDirectory()
+            ? spec.path
+            : null
+    );
+};
+
+const childDirectoryItemPromiseCallback = function (path, resolve, reject) {
+    Fs.stat(
+        path,
+        partApply(
+            childDirectoryItemStatCallback,
+            {path, promise: {resolve, reject}}
+        )
+    );
+};
+
+const childDirectoryItemsMapCallback = function (spec, dirItem) {
+    return new Promise(
+        partApply(
+            childDirectoryItemPromiseCallback,
+            Path.resolve(spec.path, dirItem)
+        )
+    );
+};
+
+const childDirectoriesReaddirResolver = function (resolve, dirItems) {
+    resolve(dirItems.filter((item) => item !== null));
+};
+
+const childDirectoriesReaddirCallback = function (spec, ignore, dirItems) {
+    Promise.all(
+        dirItems.map(
+            partApply(
+                childDirectoryItemsMapCallback,
+                spec
+            )
+        )
+    )
+        .then(
+            partApply(
+                childDirectoriesReaddirResolver,
+                spec.promise.resolve
+            )
+        );
+};
+
+const childDirectoriesPromiseCallback = function (path, resolve, reject) {
+    Fs.readdir(
+        path,
+        partApply(
+            childDirectoriesReaddirCallback,
+            {path, promise: {reject, resolve}}
+        )
+    );
+};
+
+const getChildDirectories = function (path) {
+    return new Promise(partApply(childDirectoriesPromiseCallback, path));
+};
+
+const getBottomDirectoriesResolver = function (spec, childDirectories) {
+    return childDirectories.length === 0
+        ? spec.path
+        : Promise.all(
+            childDirectories
+                .map((dir) => spec.getBottomDirectories(dir))
+        );
+};
+
+const getBottomDirectories = function _getBottomDirectories(path) {
+    return getChildDirectories(path)
+        .then(
+            partApply(
+                getBottomDirectoriesResolver,
+                {
+                    getBottomDirectories: _getBottomDirectories,
+                    path
+                }
+            )
+        );
+};
+
+getBottomDirectories("./board")
+    .then((dirs) => flatten(dirs))
+    .then((dirs) => dirs.forEach((dir) => watch(dir)));
